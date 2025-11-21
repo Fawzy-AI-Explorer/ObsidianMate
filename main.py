f968d3a198689e5b11486604358a7621e4c5ece4
mohammad fawzy <moha.fawzy63@gmail.com>
2025-11-21 16:21:34 +0200

    feat: Implement application lifespan management with startup and shutdown logging

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 """
 Main script for the application.
 """
-
 import os
+from contextlib import asynccontextmanager
+import logging
+from fastapi import FastAPI
+from routes import base
+from utils import get_settings
 
+logger = logging.getLogger("uvicorn")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):  # pylint: disable=[W0621]
+
+    logger.info("Application is starting up...")
+
+    # Startup
+    settings = get_settings()
+    app.state.settings = settings
+
+    yield  # The application runs here
+
+    # Shutdown
+    logger.info("Application is shutting down...")
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(base.base_router)
 
 def main():
     """Entry Point for the Program."""
     print(
         f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
     )
-
 
 if __name__ == "__main__":
     main()

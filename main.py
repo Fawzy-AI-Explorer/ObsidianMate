e8a83b04f8e826eac8069da5d2d59db1219fdca5
Ad7amstein <adham32003200@gmail.com>
2025-11-21 22:10:30 +0200

    feat: add data router to main application and update settings with retry attempts

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 """
 Main script for the application.
 """
+
 import os
 from contextlib import asynccontextmanager
 import logging
 from fastapi import FastAPI
-from routes import base
+from routes import base, data
 from utils import get_settings
 
 logger = logging.getLogger("uvicorn")
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):  # pylint: disable=[W0621]
@@ -24,14 +26,18 @@ async def lifespan(app: FastAPI):  # pylint: disable=[W0621]
     # Shutdown
     logger.info("Application is shutting down...")
 
+
 app = FastAPI(lifespan=lifespan)
 app.include_router(base.base_router)
+app.include_router(data.data_router)
+
 
 def main():
     """Entry Point for the Program."""
     print(
         f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
     )
+
 
 if __name__ == "__main__":
     main()

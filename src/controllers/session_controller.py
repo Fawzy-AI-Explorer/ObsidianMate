9566fc2d72ce97504f8d7f65f9555a39a969656e
mohammad fawzy <moha.fawzy63@gmail.com>
2025-11-22 11:25:44 +0200

    feat: implement delete_session method in SessionController

--- src/controllers/session_controller.py
+++ src/controllers/session_controller.py
@@ -1,27 +1,40 @@
 import os
 from controllers import BaseController
-
-
-
+from google.adk.sessions import BaseSessionService
 
 class SessionController(BaseController):
     """Controller for managing sessions."""
 
-    def __init__(self):
+    def __init__(self, session_service: BaseSessionService):
         """Initialize the SessionController."""
         super().__init__()
+        self.session_service = session_service
 
     async def create_session(self, app_name: str, user_id: str, session_id: str): 
         # create_session
         pass
 
     async def delete_session(self,  app_name: str, user_id: str, session_id: str):
-        pass
+        """
+        Delete a session by its ID.
+        Args:
+            app_name (str): The name of the application.
+            user_id (str): The ID of the user.
+            session_id (str): The ID of the session to delete.
+        Returns:
+            None
+        """
+
+        await self.session_service.delete_session(
+        app_name=app_name, 
+        user_id=user_id, 
+        session_id=session_id
+        )
+
 
     async def get_session(self,  app_name: str, user_id: str, session_id: str):
         pass
 
-    
     async def list_sessions(self, app_name, user_id=None):
         pass
 

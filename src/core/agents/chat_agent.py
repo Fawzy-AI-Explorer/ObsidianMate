13afaf4e27e463861769bda890b5ef5d14590977
Ad7amstein <adham32003200@gmail.com>
2025-11-22 16:08:58 +0200

    feat: implement ChatAgent with configuration and entry point

---
+++ src/core/agents/chat_agent.py
@@ -0,0 +1,37 @@
+import os
+from google.adk.agents import Agent
+from google.adk.models.google_llm import Gemini
+from google.genai import types
+from utils.config_utils import get_settings
+from core.tools import google_search
+from models.enums import AgentNameEnum
+from stores.llm.templates import TemplateParser
+
+app_settings = get_settings()
+template_parser = TemplateParser()
+
+retry_config = types.HttpRetryOptions(
+    attempts=app_settings.RETRY_ATTEMPS,
+    exp_base=app_settings.RETRY_EXP_BASE,
+    initial_delay=app_settings.RETRY_INITAL_DELAY,
+    http_status_codes=app_settings.RETRY_HTTP_STATUS_CODE,
+)
+
+ChatAgent = Agent(
+    name=AgentNameEnum.CHAT_AGENT,
+    model=Gemini(model=app_settings.CHATT_MODEL_NAME, retry_options=retry_config),
+    description="A simple agent that can answer general questions.",
+    instruction=template_parser.get("chat", "INSTRUCTIONS"),  # type: ignore
+    tools=[google_search],
+)
+
+
+def main():
+    """Entry Point for the Program."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()

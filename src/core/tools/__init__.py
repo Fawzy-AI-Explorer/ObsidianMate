a8fedda5653bdb1dca203590740454fd86f40f2c
Ad7amstein <adham32003200@gmail.com>
2025-11-23 20:23:59 +0200

    refactor: Standardize naming of SmartNotesPipelineTool for consistency

--- src/core/tools/__init__.py
+++ src/core/tools/__init__.py
@@ -1,6 +1,6 @@
 """Core Tools Package"""
 
 from core.tools.google_search_tool import google_search
-from core.tools.smart_notes_tool import SmartNotesPipelineTool
+from core.tools.smart_notes_tool import smart_notes_pipeline_tool
 
-__all__ = ["google_search", "SmartNotesPipelineTool"]
+__all__ = ["google_search", "smart_notes_pipeline_tool"]

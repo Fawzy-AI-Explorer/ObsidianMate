0c24994752cc5acdefaa96d4d82d992ee10310fa
mohammad fawzy <moha.fawzy63@gmail.com>
2025-11-23 21:05:49 +0200

    fix: Correct author reference and improve conversation extraction logic

--- src/core/tools/conversation_extraction_tool.py
+++ src/core/tools/conversation_extraction_tool.py
@@ -17,14 +17,22 @@ def extract_conversation(session: Session):
     """
 
     session_events = session.events # List of Event objects
-    # conversation = []
-    # for event in session_events:
-    #     conversation.append(
-    #         {
-    #             "author": event.author, # str
-    #             "event":
-    #         }
-    #     )
+    conversation = []
+    author = "unknown"
+    text = "no text"
+    for event in session_events:
+        if event.auther:
+                author = event.author
+
+        if event.content and event.content.parts:
+            if (
+                event.content.parts[0].text != "None"
+                and event.content.parts[0].text
+            ):
+                text = event.content.parts[0].text
+
+        conversation.append({"author": author, "text": text})
+
     if not session_events:
         return {"status": "error", "eror_message": "No events found in the session."}
     return {"status": "success", "session_events": session_events}

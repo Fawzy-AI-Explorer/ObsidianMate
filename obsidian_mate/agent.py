fd14222b944cf875c49f0d32eb5b08f27733e839
Ad7amstein <adham32003200@gmail.com>
2025-11-25 02:44:38 +0200

    feat: Add main entry point for the agent module

---
+++ obsidian_mate/agent.py
@@ -0,0 +1,13 @@
+import os
+from core.obsidian_mate.agent import root_agent
+
+
+def main():
+    """Entry Point for the Program."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()

0a46b3530781ae55b769c11a01bdd107d8278d50
mohammad fawzy <moha.fawzy63@gmail.com>
2025-11-29 10:30:42 +0200

    feat: Add Excalidraw and Obsidian interaction agent implementations

---
+++ src/core/obsidian_mate/sub_agents/obsidian_interaction_agent.py
@@ -0,0 +1,43 @@
+import os
+from google.adk.agents import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.models.lite_llm import LiteLlm
+from google.genai import types
+
+from models.enums import AgentNameEnum
+from stores.llm.templates import TemplateParser
+from utils.config_utils import get_settings
+from core.tools.obsidian_interaction_tool import obsidian_tool
+
+
+app_settings = get_settings()
+template_parser = TemplateParser()
+
+retry_config = types.HttpRetryOptions(
+    attempts=app_settings.RETRY_ATTEMPS,
+    exp_base=app_settings.RETRY_EXP_BASE,
+    initial_delay=app_settings.RETRY_INITAL_DELAY,
+    http_status_codes=app_settings.RETRY_HTTP_STATUS_CODE,
+)
+
+obsidian_interaction_agent = Agent(
+    name=AgentNameEnum.OBSIDIAN_INTERACTION_AGENT,
+    # model=Gemini(model=app_settings.CHAT_MODEL_NAME, retry_options=retry_config),
+    model=LiteLlm(app_settings.DEFAULT_MODEL_NAME),
+    description="A simple agent that can manage obsigian interactions.",
+    instruction=template_parser.get("interact_obsidian", "INSTRUCTIONS"),  # type: ignore
+    tools=[
+        obsidian_tool
+    ],
+)
+
+
+def main():
+    """Entry Point for the Program."""
+    print(
+        f"Welcome from `{os.path.basename(__file__).split('.')[0]}` Module. Nothing to do ^_____^!"
+    )
+
+
+if __name__ == "__main__":
+    main()
